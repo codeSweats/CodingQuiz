c8799d4c04ad1d6b9f754da390ea4c991d0b687f
G Code <seanlfrancis@gmail.com>
2021-03-16 18:58:44 -0500

    Dummy questions are added in javascript

---
+++ script.js
@@ -0,0 +1,26 @@
+//List of all qustions, choices
+let questions = [
+    {
+        q: "q1",
+        answerchoices: ["a1", "b1", "c1", "d1"],
+        a: "b1"
+    },
+
+    {
+        q: "q2",
+        answerchoices: ["a2", "b2", "c2", "d2"],
+        a: "c2"
+    },
+
+    {
+        q: "q3",
+        answerchoices: ["a3", "b3", "c3", "d3"],
+        a: "a3"
+    },
+
+    {
+        q: "q4",
+        answerchoices: ["a4", "b4", "c4", "d4"],
+        a: "d4"
+    }
+];

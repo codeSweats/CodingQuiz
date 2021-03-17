6b99af334e328599867ad2af8f90b66afa1e4c80
G Code <seanlfrancis@gmail.com>
2021-03-16 23:58:27 -0500

    Incomplete with a few more revisions

--- script.js
+++ script.js
@@ -22,5 +22,32 @@ let questions = [
         q: "q4",
         answerchoices: ["a4", "b4", "c4", "d4"],
         a: "d4"
+    },
+
+    {
+        q: "q5",
+        answerchoices: ["a5", "b5", "c5", "d5"],
+        a: "c5"
     }
 ];
+
+// Some of the global variables
+let questionindex = ;
+let gametime = 100;
+let timerid = ;
+let startBtn = document.querySelector("#start-button");
+
+
+//Variables to reference DOM elements
+let quizEl = document.getElementById("quiz");
+let timerEl = document.getElementById("remaining-time");
+let answerchoicesEl = document.getElementById("multiple-choice");
+let submitbtnEl = document.getElementById("submit-initials");
+let startbtnEl = document.getElementById("start-button");
+let initialsEl = document.getElementById("initials");
+
+for(var i=0; i < questions.length; i++){
+    var response = (questions[i]);
+}
+
+startBtn.addEventListener("click", )

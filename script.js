8c26547ea590d622436a17a14fe41ac890cbeb4c
G Code <seanlfrancis@gmail.com>
2021-03-17 19:02:30 -0500

    Added javascipt for answer choices

--- script.js
+++ script.js
@@ -47,6 +47,7 @@ let startbtnEl = document.getElementById("start-button");
 let initialsEl = document.getElementById("initials");
 let quizresultsEl = document.getElementById("quiz-results");
 let startPageEl = document.getElementById("start-page");
+let questionEl = document.getElementById("question");
 
 function setTime() {
     // Update time
@@ -75,9 +76,69 @@ function beginQuiz () {
     //Showtime now
     timerEl.textContent = gameTime;
 
+    //Display question
+    displayNextQuestion();
 }
 function finalScore() {
 
+}
+
+function displayNextQuestion () {
+    // Get current question from array
+    let nextQuestion = questions[questionIndex];
+
+    questionEl.textContent = nextQuestion.q;
+    answerchoicesEl.textContent = "";
+
+    //Display all choices
+    nextQuestion.answerchoices.forEach(function(choice, j){
+        // Create a new button for each answer choice
+        let choiceNode = document.createElement("button");
+        choiceNode.setAttribute("class", "multiple-choice");
+        choiceNode.setAttribute("class", "quizstyle");
+        choiceNode.setAttribute("value", choice);
+
+        choiceNode.textContent = j + 1 + ". " + choice;
+
+        // Attach click event listener to each choice
+
+        choiceNode.onclick = choiceClick;
+        
+        //Display the choice node here
+        answerchoicesEl.appendChild(choiceNode);
+    });
+}
+
+function choiceClick() {
+    // Check if user chose wrong asnwer and give penalty
+    if (this.value !== questions[questionIndex].a) {
+        gameTime -= 10;
+
+        if (gameTime < 0) {
+            gameTime = 0;
+        }
+        // Display new time on page
+        timerEl.textContent = gameTime;
+        quizresultsEl.textContent = "wrong!";
+
+    } else {
+        quizresultsEl.textContent = "Correct!"
+    }
+
+    // Show write or wrong for only 1 second
+    quizresultsEl.setAttribute("class", "quiz-results");
+    setTimeout(function() {
+        quizresultsEl.setAttribute("class", "quiz-results hidden");
+    }, 1000);
+
+    questionIndex++;
+
+    if (questionIndex === questions.length) {
+        finishQuiz();
+
+    } else {
+        displayNextQuestion();
+    }
 }
 
 for(var i=0; i < questions.length; i++){
